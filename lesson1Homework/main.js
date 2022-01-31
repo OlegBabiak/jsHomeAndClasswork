cb9472a7667fe5247846c56158d23a5795fdd030
OlegBabiak <ovbabiak@gmail.com>
2022-01-31 23:25:07 +0200

    Commit Lesson 1 js homework

---
+++ lesson1Homework/main.js
@@ -0,0 +1,169 @@
+let string1 = 'hello';
+let string2 = 'owu';
+let string3 = 'com';
+let string4 = 'ua';
+
+let int1 = 1;
+let int2 = 10;
+let int3 = -999;
+let int4 = 123;
+
+let fl1 = 3.14;
+let fl2 = 2.7;
+let fl3 = 16;
+
+let bool1 = true;
+let bool2 = false;
+
+console.log(string1);
+console.log(string2);
+console.log(string3);
+console.log(string4);
+console.log(int1);
+console.log(int2);
+console.log(int3);
+console.log(int4);
+console.log(fl1);
+console.log(fl2);
+console.log(fl3);
+console.log(bool1);
+console.log(bool2);
+
+alert(string1);
+alert(string2);
+alert(string3);
+alert(string4);
+alert(int1);
+alert(int2);
+alert(int3);
+alert(int4);
+alert(fl1);
+alert(fl2);
+alert(fl3);
+alert(bool1);
+alert(bool2);
+
+document.write('string1');
+document.write('string2');
+document.write('string3');
+document.write('string4');
+document.write('int1');
+document.write('int2');
+document.write('int3');
+document.write('int4');
+document.write('fl1');
+document.write('fl2');
+document.write('fl3');
+document.write('bool1');
+document.write('bool2');
+
+string1 = 'by';
+string2 = 'prometheus';
+string3 = 'tv';
+string4 = 'pl';
+int1 = 2;
+int2 = 30;
+int3 = 777;
+int4 = 321;
+fl1 = 4.13;
+fl2 = 7.2;
+fl3 = 61;
+bool1 = false;
+bool2 = true;
+
+
+console.log(string1);
+console.log(string2);
+console.log(string3);
+console.log(string4);
+console.log(int1);
+console.log(int2);
+console.log(int3);
+console.log(int4);
+console.log(fl1);
+console.log(fl2);
+console.log(fl3);
+console.log(bool1);
+console.log(bool2);
+
+alert(string1);
+alert(string2);
+alert(string3);
+alert(string4);
+alert(int1);
+alert(int2);
+alert(int3);
+alert(int4);
+alert(fl1);
+alert(fl2);
+alert(fl3);
+alert(bool1);
+alert(bool2);
+
+document.write('string1');
+document.write('string2');
+document.write('string3');
+document.write('string4');
+document.write('int1');
+document.write('int2');
+document.write('int3');
+document.write('int4');
+document.write('fl1');
+document.write('fl2');
+document.write('fl3');
+document.write('bool1');
+document.write('bool2');
+
+let firstName = 'Oleg';
+let middleName = 'Volodymyrovych';
+let lastName = 'Babiak';
+let person = firstName + ' ' + middleName + ' ' + lastName;
+
+let name = prompt();
+let mName = prompt();
+let age = prompt();
+
+document.write('Вітаю '+name + ' ' + mName + '. Тобі ' + age + ' років');
+
+
+let a1 = 100;
+let b = '100';
+let c = true;
+console.log(typeof a1);
+console.log(typeof b);
+console.log(typeof c);
+
+
+// 5 ? 6 -> true
+console.log(5 < 6);
+// 5 ? 6 -> false
+console.log(5 > 6);
+// 5 ? 6 -> false
+console.log(5 === 6);
+// 5 ? 6 -> false
+console.log(5 >= 6);
+
+// 10 ? 10 -> true
+console.log(10 >= 10);
+// 10 ? 10 -> true
+console.log(10 === 10);
+// 10 ? 10 -> false
+console.log(10 < 10);
+// 10 ? 10 -> false
+console.log(10 !== 10);
+// 10 ? 10 -> false
+console.log(10 > 10);
+
+// 123 ? '123' -> false
+console.log(123 === '123');
+// 123 ? '123' -> true
+console.log(123 == '123');
+
+let str = "20";
+let a = 5;
+document.write(str + a + "<br/>");      //  число a приведено до String, потім до 20 конкантиновано 5 результат: 205;
+document.write(str - a + "<br/>");      // String str приведено до int, потім 20 - 5 результат: 15;
+document.write(str * "2" + "<br/>");    // String str і "2" приведено до int, потім 20 * 2 результат: 40;
+document.write(str / 2 + "<br/>");      // String str приведено до int, потім 20 / 2 результат: 10;
+
+
